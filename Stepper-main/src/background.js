08ea42f1f36313b344625461bbc130c98241f450
MikkelGjessing <mikkel.gjessing@nexigroup.com>
2026-02-11 18:10:17 +0100

    Delete Stepper-main directory

--- Stepper-main/src/background.js
+++
@@ -1,6 +0,0 @@
-// Background Service Worker
-// Handles extension lifecycle and side panel opening
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.sidePanel.open({ windowId: tab.windowId });
-});
